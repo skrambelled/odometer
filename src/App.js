9ea5fd125c4cf4ff27775bf9747416caa83c78ba
Mark Bell <skrambelled@gmail.com>
2021-01-04 13:56:48 -0800

    added basic odometer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header text="This is an odometer!"/>
+        <Odometer />
+        <Footer trademark="Mark Bell"/>
     </div>
   );
+}
+
+function Header(props) {
+    return (
+        <nav>
+            <h2>{props.text}</h2>
+        </nav>
+    )
+}
+
+function Footer(props) {
+    return (
+        <footer>
+            <h3>{props.trademark}&trade;</h3>
+        </footer>
+    )
+}
+
+class Odometer extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            mileage : 0,
+        }
+    }
+
+    addMileage(additionalMiles) {
+        this.setState({
+            mileage : (this.state.mileage + additionalMiles) % 10000
+        });
+    }
+
+    render() {
+        return (
+            <>
+            <h2>Odemeter: {this.state.mileage.toString().padStart(4, "0")}</h2>
+            <button onClick={() => this.addMileage(1)}>+1</button>
+            <button onClick={() => this.addMileage(10)}>+10</button>
+            <button onClick={() => this.addMileage(100)}>+100</button>
+            <button onClick={() => this.addMileage(1000)}>+1000</button>
+            </>
+        );
+    }
 }
 
 export default App;
